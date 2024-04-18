02cf0b222833e40497f82aa36248f34ef8e5b4db
Zhiwei Bao <452858265@qq.com>
2024-04-18 15:10:25 +0800

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -1,6 +1,6 @@
 # GGMP
 
-redbiom search metadata "feces where qiita_study_id == 11757" | grep -vi “blank” | grep -vi “BLANK” > GGMP-samples-feces.txt”
+redbiom search metadata "feces where qiita_study_id == 11757" | grep -vi “blank” | grep -vi “BLANK” > GGMP-samples-feces.txt
 redbiom fetch sample-metadata --from GGMP-samples-feces.txt --context Pick_closed-reference_OTUs-Greengenes-Illumina-16S-V4-150nt-bd7d4d --output GGMP-metadata-feces.tsv --all-columns
 redbiom fetch samples --from GGMP-samples-feces.txt --context Pick_closed-reference_OTUs-Greengenes-Illumina-16S-V4-150nt-bd7d4d --output GGMP-feces.biom
 
